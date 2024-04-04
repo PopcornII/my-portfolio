7463b0f33b2a200979ec4a548caf236fdf7a0bb3
Sok Rithy <109121172+PopcornII@users.noreply.github.com>
2024-04-05 02:22:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import './index.css'
-import Home from "./components/Home";
+import Home from "./components/home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
